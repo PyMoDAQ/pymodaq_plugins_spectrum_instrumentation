3f351f81d25097705f90ec8774f82bbf21550377
Martin Luttmann <malo.herve@epfl.ch>
2026-02-12 10:04:11 +0100

    Revamped LockIn and visualisation

--- src/pymodaq_plugins_spectrum_instrumentation/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Spectrum_Lockin.py
+++ src/pymodaq_plugins_spectrum_instrumentation/daq_viewer_plugins/plugins_1D/daq_1Dviewer_Spectrum_Lockin.py
@@ -61,7 +61,9 @@ class DAQ_1DViewer_Spectrum_Lockin(DAQ_1DViewer_Spectrum):
         self.num_pulses = self.settings.child('timing', 'Num_Pulses').value()
         self.num_LI_period = int( self.settings.child('timing', 'Range').value() * self.LI_pulseFreq)          # LI = Lock In, LI_Period = Up or Down
         self.points_per_pulse = self.settings.child('timing', 'Sample_per_Pulse').value() # Points per pulse
-        self.pulse_per_LI_Period = int(self.num_pulses/self.num_LI_period) # Pulses per Period
+        self.pulse_per_LI_Period = self.num_pulses/self.num_LI_period # Pulses per Period
+
+        self.update_lockin_param()
 
         chan_str = [param.title() for param in self.settings.child("channels").children() if param.type()=="led_push" and param.value()]
         self.settings.child('lock_in', 'diffChannel').setLimits( chan_str )
@@ -80,8 +82,10 @@ class DAQ_1DViewer_Spectrum_Lockin(DAQ_1DViewer_Spectrum):
         if param.name()=="BG_sub":
             if param.value(): self.settings.child("lock_in", "BG_prop").show()
             else: self.settings.child("lock_in", "BG_prop").hide()
-    
 
+        if param.name() in ["LI_PulseFreq", 'Range', "sampleRate"]:     # TODO : is this called if range / sample rate is changed by another commit_setting (ie points_per_pulse)
+            self.update_lockin_param()
+    
 
     def grab_data(self, Naverage=1, **kwargs):
         """ Start a grab from the detector
@@ -117,14 +121,17 @@ class DAQ_1DViewer_Spectrum_Lockin(DAQ_1DViewer_Spectrum):
 
             self.emit_status(ThreadCommand('Update_Status', [ 'Problem During the LockIn Process ! ! ']))
             self.hit_except = True
+
+            I_a, I_Ba, I_Bd, D_a, D_Ba, D_Bd, ND_a, ND_Ba, ND_Bd, phi_a, phi_Ba, phi_Bd = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
             
 
         # --- Create Pymodaq export
         self.x_axis = Axis(data=self.controller.get_the_x_axis(), label='Time', units="s", index=0)
+        data_to_export = []
 
-        # Integrated Trace, only one to always export
-        dwa_int = DataFromPlugins(name='Pulse train', data=[diff_data_int, sum_data_int], dim='Data1D', labels=['D', 'I'], do_plot=self.settings.child('lock_in', 'PlotSave', 'PulseTrain').value())
-        data_to_export = [dwa_int]
+        # # Integrated Trace, only one to always export
+        # dwa_int = DataFromPlugins(name='Pulse train', data=[diff_data_int, sum_data_int], dim='Data1D', labels=['D', 'I'], do_plot=self.settings.child('lock_in', 'PlotSave', 'PulseTrain').value())
+        # data_to_export.append(dwa_int)
 
         # Also deal with Trace separately as it is 1D
         if self.settings.child('lock_in', 'PlotSave', 'Trace').value():
@@ -132,7 +139,9 @@ class DAQ_1DViewer_Spectrum_Lockin(DAQ_1DViewer_Spectrum):
             # data_to_export.append(dwa_trace)
 
             # Add visualisation
-            data_trace = np.sign( np.sin( self.settings.child('lock_in', 'LI_PulseFreq').value() * self.x_axis.get_data()) ) * np.max( np.abs(data_tot) )
+            # step = self.x_axis.get_data() < int(self.num_lockin_steps*self.points_per_step)
+            step = np.arange(len(data_tot[0]))< int(self.num_lockin_steps*self.points_per_step)
+            data_trace = np.sign( np.sin( 2*np.pi*self.settings.child('lock_in', 'LI_PulseFreq').value() * self.x_axis.get_data()) ) * np.max( np.abs(data_tot) ) * step
             dwa_trace = DataFromPlugins(name='Trace', data=data_tot + [data_trace], dim='Data1D', labels=self.controller.activated_str+ ["Lock In Trace"], axes=[self.x_axis])
             data_to_export.append(dwa_trace)
 
@@ -148,12 +157,62 @@ class DAQ_1DViewer_Spectrum_Lockin(DAQ_1DViewer_Spectrum):
 
 
 
-
     def lock_in(self, diff_data, sum_data):
         """
         From 2 traces, calculate all relevant values
         """
 
+        # --- Reshape to correct size
+        diff_data_shortened = diff_data[: int(self.num_lockin_steps*self.points_per_step ) ]
+        sum_data_shortened = sum_data[: int(self.num_lockin_steps*self.points_per_step) ]
+
+        # --- Integrate Pulses and remove Background
+        diff_chan_reshaped = diff_data_shortened.reshape(self.num_lockin_steps, self.points_per_step)
+        sum_chan_reshaped = sum_data_shortened.reshape(self.num_lockin_steps, self.points_per_step)
+
+        if self.settings.child('lock_in', 'BG_sub').value() == True:
+            cutoff = int(self.points_per_step * self.settings.child("lock_in", "BG_prop").value()/100)
+            diff_data_int = np.sum( diff_chan_reshaped[:,:cutoff], axis=1 ) * (1-self.settings.child('lock_in', 'BG_sub').value()/100) - np.sum(diff_chan_reshaped[:,cutoff:], axis=1) * self.settings.child('lock_in', 'BG_sub').value()/100
+            sum_data_int = np.sum( sum_chan_reshaped[:,:cutoff], axis=1 ) * (1-self.settings.child('lock_in', 'BG_sub').value()/100) - np.sum(sum_chan_reshaped[:,cutoff:], axis=1) * self.settings.child('lock_in', 'BG_sub').value()/100
+        else:
+            diff_data_int = np.sum(diff_chan_reshaped, axis=1)
+            sum_data_int = np.sum(sum_chan_reshaped, axis=1)
+
+        # - Compute I_Ba and I_Bd
+        I_Bd_list = sum_data_int[::2] - sum_data_int[1::2]
+        I_Bd = np.mean(I_Bd_list)
+        I_Ba = np.mean(sum_data_int)
+
+
+        # - Compute D_Ba and D_Bd
+        D_Bd_list =   diff_data_int [::2] - diff_data_int [1::2]
+        D_Bd = np.mean(D_Bd_list)
+        D_Ba = np.mean(diff_data_int)
+
+
+        # - Compute ND_Ba and ND_Bd
+        ND_int = np.divide(diff_data_int, sum_data_int) / self.settings.child('lock_in', 'Gain').value() / 2
+        ND_a = np.mean(ND_int)
+
+        ND_Bd_list = ND_int[::2] - ND_int[1::2]
+        ND_Bd = np.mean(ND_Bd_list)
+        ND_Ba = np.mean(ND_int)
+
+        # - Computing phi_Ba and phi_Bd
+        phi_a = ND_a / self.settings.child('lock_in', 'Conversion').value()
+        phi_Ba = ND_Ba / self.settings.child('lock_in', 'Conversion').value()
+        phi_Bd = ND_Bd / self.settings.child('lock_in', 'Conversion').value()
+
+        return sum_data_int, 0, I_Ba, I_Bd, diff_data_int, 0, D_Ba, D_Bd, ND_a, ND_Ba, ND_Bd, phi_a, phi_Ba, phi_Bd
+        # return sum_data,       0,  0,      0,   diff_data,      0,  0,      0,  0,      0,   0,        0,   0,      0
+
+
+
+
+    def lock_in_backup(self, diff_data, sum_data):
+        """
+        From 2 traces, calculate all relevant values
+        """
 
         # --- Reshape to correct size (TODO: way of avoiding ? Or just expected that num sample != actual num samples )
         diff_data = diff_data[: int(self.num_pulses*self.points_per_pulse) ]
@@ -163,21 +222,21 @@ class DAQ_1DViewer_Spectrum_Lockin(DAQ_1DViewer_Spectrum):
         diff_chan_reshaped = diff_data.reshape(self.num_pulses, self.points_per_pulse)
         sum_chan_reshaped = sum_data.reshape(self.num_pulses, self.points_per_pulse)
 
-        if self.settings.child('lock_in', 'BG_sub').value() == True:    # TODO : Make background subtraction more smart
+        if self.settings.child('lock_in', 'BG_sub').value() == True:
             cutoff = int(self.points_per_pulse * self.settings.child("lock_in", "BG_prop").value()/100)
             diff_data_int = np.sum( diff_chan_reshaped[:,:cutoff], axis=1 ) * (1-self.settings.child('lock_in', 'BG_sub').value()/100) - np.sum(diff_chan_reshaped[:,cutoff:], axis=1) * self.settings.child('lock_in', 'BG_sub').value()/100
             sum_data_int = np.sum( sum_chan_reshaped[:,:cutoff], axis=1 ) * (1-self.settings.child('lock_in', 'BG_sub').value()/100) - np.sum(sum_chan_reshaped[:,cutoff:], axis=1) * self.settings.child('lock_in', 'BG_sub').value()/100
-
-
-
         else:
             diff_data_int = np.sum(diff_chan_reshaped, axis=1)
             sum_data_int = np.sum(sum_chan_reshaped, axis=1)
 
         # - Compute I_a and D_a
-
         D_a = np.mean(diff_data_int)
         I_a = np.mean(sum_data_int)
+
+
+        # --- Reshape into a correct number of pulse that works with LockIn
+
 
 
         # - Compute I_Ba and I_Bd
@@ -218,6 +277,14 @@ class DAQ_1DViewer_Spectrum_Lockin(DAQ_1DViewer_Spectrum):
         phi_Bd = ND_Bd / self.settings.child('lock_in', 'Conversion').value()
 
         return sum_data_int, I_a, I_Ba, I_Bd, diff_data_int, D_a, D_Ba, D_Bd, ND_a, ND_Ba, ND_Bd, phi_a, phi_Ba, phi_Bd
+
+    def update_lockin_param(self):
+        self.lockin_step_duration = (1/self.settings.child("lock_in", "LI_PulseFreq").value()) / 2                      # Divide by 2 since one step is defined as only up or down
+        self.num_lockin_steps = int(self.settings.child("timing", 'Range').value()*1e-3 / self.lockin_step_duration  /2)*2         #
+        self.points_per_step = int( self.lockin_step_duration * round(self.settings.child("timing", "sampleRate").value() * 1e6) )
+
+        print(self.lockin_step_duration*1e3, 'ms', '-', self.num_lockin_steps, '-', self.points_per_step, '- (',   round(self.settings.child("timing", "sampleRate").value() * 1e6))
+
 
 
 
